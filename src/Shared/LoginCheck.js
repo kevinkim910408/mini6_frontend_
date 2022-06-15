1fb2b3f0e4c8a6a233ea9038f96883616030bcef
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-06-15 18:32:28 -0400

    edit: 정규식 서버정규식하고 통일

--- src/Shared/LoginCheck.js
+++ src/Shared/LoginCheck.js
@@ -2,18 +2,19 @@
 
 // 아이디 형식 검사
 export const idCheck = (username) => {
-  let _reg = /^[가-힣a-zA-Z0-9-_.]{2,10}$/;
-
+  let _reg = /^(?=.*[0-9a-zA-Z])[가-힣a-zA-Z0-9-_.]{4,15}$/;
   return _reg.test(username);
 };
+
 // 비밀번호 체크
-
 export const passwordCheck = (passwordcheck) => {
-  let regPass = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{6,20}$/;
-
+  let regPass = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{4,20}$/;
   return regPass.test(passwordcheck);
 };
 // 정규식
 // ^ => 시작
 // (?=.*\d) => 0~9까지의 숫자 표현
 // (?=.*[a-zA-Z]) => 알파벳
+
+// 숫자 영어 필수 4-15
+// 비번 영어숫자특수문자 필수 4-20
--- src/Shared/api.js
+++ src/Shared/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://13.124.63.214:8080"
+    baseURL: "http://13.209.88.160:8080"
 })
 
 // 만원님 서버
